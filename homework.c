d3bd2374d530af810077fc803599c19ee4e55f34
Razvan Radoi <a-rarad@microsoft.com>
2018-12-16 13:28:29 +0200

    add first iteration for bw imgae

--- homework.c
+++ homework.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define IMAGE_SIZE_BW 1
 #define IMAGE_SIZE_COL 3
@@ -36,7 +37,6 @@ const double sharpenFilter[3][3] = {{0, -2.0 / 3, 0},
                                     {0, -2.0 / 3, 0}};
 
 const double meanRemovalFilter[3][3] = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
-
 const double embossFilter[3][3] = {{0, 1, 0}, {0, 0, 0}, {0, -1, 0}};
 
 void readInput(const char * fileName, image *img) {
@@ -94,7 +94,7 @@ void readInput(const char * fileName, image *img) {
     if (tempImageType[1] == '5') {  // image is b&w
         img->bwData = (unsigned char **) malloc(heightAsNum * sizeof(unsigned char *));
         for (int i = 0; i < heightAsNum; i++) {
-            img->bwData[i] = (unsigned char *) malloc(widthAsNum * sizeof(unsigned char *));
+            img->bwData[i] = (unsigned char *) malloc(widthAsNum * sizeof(unsigned char));
         }
 
         unsigned char *buffer = (unsigned char *)malloc(widthAsNum * heightAsNum);
@@ -223,34 +223,87 @@ void writeData(const char * fileName, image *img) {
 
 int main(int argc, char * argv[]) {
     // read input data in image file
-    image image;
-    readInput(argv[1], &image);
-
-    if (image.type == BW) {  // image is bw
-        int rank, size;
-        MPI_Init (&argc, &argv);
-        MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-        MPI_Comm_size (MPI_COMM_WORLD, &size);
+    image givenImage;
+    readInput(argv[1], &givenImage);
+    printf("here\n");
+    if (givenImage.type == BW) {  // image is bw
+        // init a temp image to work with
+        image temp;
+        temp.bwData = (unsigned char **)malloc(givenImage.height * sizeof(unsigned char *));
+        for (int i = 0; i < givenImage.height; i++)
+        {
+            temp.bwData[i] = (unsigned char *)malloc(givenImage.width * sizeof(unsigned char));
+        }
+        printf("here\n");
 
         // for each filter
         for (int i = FILTER_START; i < FILTER_END; i++) {
-            if (strcmp(argv[i],"smooth") == 0) {
+            // copy image data to temp image
+            printf("here\n");
 
-            } else
-            if (strcmp(argv[i],"blur") == 0) {
-
-            } else
-            if (strcmp(argv[i],"sharpen") == 0) {
-
-            } else
-            if (strcmp(argv[i],"mean") == 0) {
-
-            } else
-            if (strcmp(argv[i],"emboss") == 0) {
-
+            for (int j = 0; j < givenImage.height; j++) {
+                memcpy(temp.bwData[j], givenImage.bwData[j], givenImage.width * sizeof(unsigned char));
             }
+
+            // set the filter;
+            double filter[3][3];
+            if (strcmp(argv[i], "smooth") == 0) {
+                memcpy(filter, smoothingFilter, 9 * sizeof(double));
+            }
+            else if (strcmp(argv[i], "blur") == 0) {
+                memcpy(filter, gaussBlurFilter, 9 * sizeof(double));
+            }
+            else if (strcmp(argv[i], "sharpen") == 0) {
+                memcpy(filter, sharpenFilter, 9 * sizeof(double));
+            }
+            else if (strcmp(argv[i], "mean") == 0) {
+                memcpy(filter, meanRemovalFilter, 9 * sizeof(double));
+            }
+            else if (strcmp(argv[i], "emboss") == 0) {
+                memcpy(filter, embossFilter, 9 * sizeof(double));
+            }
+            printf("here + %s\n", argv[i]);
+
+            // start the threads
+            int rank, size;
+            MPI_Init(&argc, &argv);
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+            MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+            // set the responsability of a thread
+            int mulFactor = (int)ceil((1.0 * givenImage.width * givenImage.height) / size);
+            int lowBound = mulFactor * rank;
+            int highBound = (int)fmin(mulFactor * (rank + 1), givenImage.width * givenImage.height);
+            // eaech thread edits the pixels assigned to it
+            for (int j = lowBound; j < highBound; j++) {
+                int line = j / givenImage.width;
+                int column = j % givenImage.width;
+                printf("%d %d\n", line, column);
+
+                // do not touch border pixels
+                if (line > 1 && column > 1 && line < givenImage.height - 1 && column < givenImage.width - 1) {
+                    givenImage.bwData[line][column] = filter[(line) % 3][(column - 1) % 3] * givenImage.bwData[line][column - 1] +
+                                               filter[(line) % 3][(column) % 3] * givenImage.bwData[line][column] +
+                                               filter[(line) % 3][(column + 1) % 3] * givenImage.bwData[line][column + 1] +
+                                               filter[(line - 1) % 3][(column - 1) % 3] * givenImage.bwData[line - 1][column - 1] +
+                                               filter[(line - 1) % 3][(column) % 3] * givenImage.bwData[line - 1][column] +
+                                               filter[(line - 1) % 3][(column + 1) % 3] * givenImage.bwData[line - 1][column + 1] +
+                                               filter[(line + 1) % 3][(column - 1) % 3] * givenImage.bwData[line + 1][column - 1] +
+                                               filter[(line + 1) % 3][(column) % 3] * givenImage.bwData[line + 1][column] +
+                                               filter[(line + 1) % 3][(column + 1) % 3] * givenImage.bwData[line + 1][column + 1];
+                }
+            }
+            // join the threads
+            MPI_Finalize();
         }
-        MPI_Finalize();
+        printf("here\n");
+
+        // clean-up temp image data
+        for (int i = 0; i < givenImage.height; i++) {
+            free(temp.bwData[i]);
+        }
+        free(temp.bwData);
+        printf("here\n");
     } else {  // image is in color
         int rank, size;
         MPI_Init (&argc, &argv);
@@ -277,9 +330,11 @@ int main(int argc, char * argv[]) {
         }
         MPI_Finalize();
     }
+    printf("here\n");
 
     // write the output data (and free image allocated space)
-    writeData(argv[2], &image);
+    writeData(argv[2], &givenImage);
+    printf("here\n");
 
     return 0;
 }
